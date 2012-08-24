4d089a22a307eebd5c1314dceda921263e4a402d
x_ono_sendai_x <nick@indigorenderer.com>
2012-08-24 16:39:51 +0000

    Added my name to a couple of headers.

--- sources/indigo/export/geometry.py
+++ sources/indigo/export/geometry.py
@@ -7,7 +7,7 @@
 # --------------------------------------------------------------------------
 #
 # Authors:
-# Doug Hammond
+# Doug Hammond, Nicholas Chapman
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
