426c46033301705e4c247e42a21f77230ca5c3c4
x_ono_sendai_x@yahoo.com <x_ono_sendai_x@yahoo.com@fc0f8538-35e5-4669-c1cd-38a0e968f54e>
2012-08-24 16:39:51 +0000

    Added my name to a couple of headers.

--- trunk/sources/indigo/__init__.py
+++ trunk/sources/indigo/__init__.py
@@ -7,7 +7,7 @@
 # --------------------------------------------------------------------------
 #
 # Authors:
-# Doug Hammond, Thomas Ludwig
+# Doug Hammond, Thomas Ludwig, Nicholas Chapman
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
