bf3d26d6000ba1758f9cc640ccf93df371abea37
yvescolle <yves@indigorenderer.com>
2013-04-11 17:06:02 +0000

    * Updated Changelog.txt. * Bumped version to 3.6.10.0.

--- sources/indigo/__init__.py
+++ sources/indigo/__init__.py
@@ -28,7 +28,7 @@
 bl_info = {
 	"name": "Blendigo",
 	"author": "Glare Technologies Ltd.",
-	"version": (3, 6, 5, 1), # Script version
+	"version": (3, 6, 10, 0), # Script version
 	"blender": (2, 6, 4), # The minimum Blender version required to run the script
 	"api": 44136,
 	"category": "Render",
