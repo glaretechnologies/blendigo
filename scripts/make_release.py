28fd0f30a63409c5d2c288126efe105b76a6b7ce
x_ono_sendai_x <nick@indigorenderer.com>
2013-07-05 12:05:24 +0000

    Updated release script.

--- scripts/make_release.py
+++ scripts/make_release.py
@@ -19,22 +19,7 @@ if __name__ == '__main__':
 		
 		ZIP_NAME = "blendigo-2.6-%s.zip" % TAG
 		
-		# update code to tagged version
-		#subprocess.call("hg update -r %s" % TAG)
-		
-		# run regression test
-		# del os.environ['BLENDIGO_RELEASE']
-		# rt_path = os.path.realpath( os.getcwd()+r'\..\..\blendigo-2.5-regressiontest')
-		# sys.path.append(rt_path)
 		cwd = os.getcwd()
-		# os.chdir(rt_path)
-		# from run import regression_test
-		# output_log, failure_count = regression_test(None, TAG)
-		# os.chdir(cwd)
-		# rt_log = '\n'.join(output_log)
-		# print(rt_log)
-		# if failure_count > 0:
-			# raise Exception('%i regression tests failed, or had high MSE values' % failure_count)
 		
 		# package the tagged version
 		from make_addon import make_addon
@@ -42,24 +27,21 @@ if __name__ == '__main__':
 		
 		# Make windows installer
 		os.environ['BLENDIGO_VERSION'] = TAG
-		os.environ['BLENDER_VERSION'] = '%i.%i%i' % bl_info['blender']
+
+		# Make a Blender version string string like "2.6x"
+		os.environ['BLENDER_VERSION'] = str(bl_info['blender'][0]) + "." + str(bl_info['blender'][1]) + "x"
 		INSTALLER_NAME = "blendigo-2.6-%s-installer.exe" % TAG
 		os.chdir('./installer_windows')
-		subprocess.call("makensis blendigo-25.nsi")
+		subprocess.call("makensis /V2 blendigo-25.nsi")
 		os.chdir(cwd)
 		del os.environ['BLENDIGO_VERSION']
 		del os.environ['BLENDER_VERSION']
 		
 		if os.path.exists(ZIP_NAME):
-			# use 3rd argument as debug switch; prevents upload and emails
-			# upload the release and notify via email
-			#subprocess.call("pscp -i \"%(UP)s/.ssh/id_rsa.ppk\" %(ZN)s %(UN)s@indigorenderer.com:/home/%(UN)s/blendigo" % {'ZN':ZIP_NAME, 'UN':SERVER_USER, 'UP': os.environ['USERPROFILE']})
-			#subprocess.call("pscp -i \"%(UP)s/.ssh/id_rsa.ppk\" installer_windows\%(IN)s %(UN)s@indigorenderer.com:/home/%(UN)s/blendigo" % {'IN': INSTALLER_NAME, 'UN':SERVER_USER, 'UP': os.environ['USERPROFILE']})
-			#subprocess.call("pscp %(ZN)s %(UN)s@indigorenderer.com:/home/%(UN)s/blendigo" % {'ZN':ZIP_NAME, 'UN':SERVER_USER, 'UP': os.environ['USERPROFILE']})
-			#subprocess.call("pscp installer_windows\%(IN)s %(UN)s@indigorenderer.com:/home/%(UN)s/blendigo" % {'IN': INSTALLER_NAME, 'UN':SERVER_USER, 'UP': os.environ['USERPROFILE']})
+			server = "downloads.indigorenderer.com"
 			
-			subprocess.call("pscp %(ZN)s %(UN)s@indigorenderer.com:/var/www/indigorenderer.com/dist/exporters/blendigo" % {'ZN':ZIP_NAME, 'UN':SERVER_USER, 'UP': os.environ['USERPROFILE']})
-			subprocess.call("pscp installer_windows\%(IN)s %(UN)s@indigorenderer.com:/var/www/indigorenderer.com/dist/exporters/blendigo" % {'IN': INSTALLER_NAME, 'UN':SERVER_USER, 'UP': os.environ['USERPROFILE']})
+			subprocess.call("pscp %(ZN)s %(UN)s@%(server)s:/var/www/dl-indigorenderer.com/dist/exporters/blendigo" % {'ZN':ZIP_NAME, 'UN':SERVER_USER, 'UP': os.environ['USERPROFILE'], 'server' : server})
+			subprocess.call("pscp installer_windows\%(IN)s %(UN)s@%(server)s:/var/www/dl-indigorenderer.com/dist/exporters/blendigo" % {'IN': INSTALLER_NAME, 'UN':SERVER_USER, 'UP': os.environ['USERPROFILE'], 'server' : server})
 			
 			#from send_release_email import send_release_email
 			#DIST_URL = "http://www.indigorenderer.com/dist/exporters/blendigo/"
@@ -74,7 +56,3 @@ if __name__ == '__main__':
 	
 	except Exception as err:
 		print('Release aborted: %s' % err)
-	
-	#finally:
-		# update to current revision
-		#subprocess.call("hg update")
